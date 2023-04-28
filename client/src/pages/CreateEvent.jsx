f7393b13285c801963b049338f0edaba7671a5de
muulinha <lucasfl.araujo@hotmail.com>
2023-04-28 16:13:07 +0800

    package json fixed

--- client/src/pages/CreateEvent.jsx
+++ client/src/pages/CreateEvent.jsx
@@ -2,20 +2,40 @@
 import React from 'react';
 // import './CreateEvent.css';
 
-// When the user clicks in "login", the page should redirect to the /home page
-// the Twitch API fecthUser and fetchStreams should be triggered to populate the home page
 
 const CreateEvent = () => {
+
+    
+
+
     return ( 
         <div className='create-event-container'>
             <div className='backboard-img backboard-login'>
                 <h1 className='login-title'>CREATE EVENT </h1>
                 <p className='login-subtitles'> Event Date:</p>
-                <input className='login-input' placeholder="Type your twitch user here..."></input>
+                <input 
+                  className='login-input' 
+                  required type="text" 
+                  id="event-date" 
+                  placeholder="dd/mm/yyyy"
+                />
+                
                 <p className='login-subtitles'> Event Title:</p>
-                <input className='login-input' placeholder="Type your password here..."></input>
+                <input 
+                  className='login-input' 
+                  required type="text" 
+                  id="event-title" 
+                  placeholder="Type the event title here..."
+                />
+
                 <p className='login-subtitles'> Event Description:</p>
-                <input className='login-input' placeholder="Type your password here..."></input>
+                <input 
+                  className='login-input' 
+                  required type="text" 
+                  id="event-description" 
+                  placeholder="Type the event description here..."
+                />
+
                 <div className='buttons-align'>
                 <div>
                     <a href='./Events'>
---
+++ server/Utils/createEvent.js
@@ -0,0 +1,4 @@
+const eventDate = document.getElementById("event-date");
+const eventTitle = document.getElementById("event-title");
+const eventDescription = document.getElementById("event-description");
+
