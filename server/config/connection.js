b24629443cc8a36b875378a06cb6134b0e7a8ae4
muulinha <lucasfl.araujo@hotmail.com>
2023-05-04 17:02:53 +0800

    Trying heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,7 +5,7 @@ const { connect, connection } = require("mongoose");
 const connectionString =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/team-updb";
 
-mongoose.connect(connectionString, {
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then((connection) => {
