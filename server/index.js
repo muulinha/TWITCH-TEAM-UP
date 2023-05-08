968d60b94f66a26f11af61f3b42fdd9699d96581
muulinha <lucasfl.araujo@hotmail.com>
2023-05-08 18:09:31 +0800

    heroku

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require("cors"); // import cors package
 const PORT = process.env.PORT || 4040;
 const app = express();
 app.use(cors()); // enable CORS
+const path = require('path');
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +14,7 @@ app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+};
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
